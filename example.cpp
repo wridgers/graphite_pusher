737d8eec25a62d9c3f695a33b8b544ad60ff89aa
Will Ridgers <will@mindfuzz.net>
2019-03-13 00:48:16 +0000

    Clarify example

--- example.cpp
+++ example.cpp
@@ -7,16 +7,19 @@ int main() {
 	const auto graphite = std::make_shared<GraphitePusher>("localhost", 2004);
 	graphite->start();
 
-	graphite->push_sample("graphite_pusher.example_1", 12.5);
+	// timestamp == now
+	graphite->push_sample("graphite_pusher.example_1", 12.345);
 
 	for (double s = 0; s < 10.0; s += 0.1) {
 		graphite->push_sample("graphite_pusher.example_2", s);
 	}
 
+	// specify timestamp
 	const auto epoch = std::chrono::system_clock::now().time_since_epoch();
 	const auto ts = std::chrono::duration_cast<std::chrono::seconds>(epoch).count();
-	graphite->push_sample("graphite_pusher.example_3", ts, 12.5);
+	graphite->push_sample("graphite_pusher.example_3", ts, 100);
 
+	// optional. block until metric queue is flushed.
 	graphite->shutdown();
 
 	return 0;
